8ab057f8bc6a2710204f99ae7b74f2091fe4207c
Jie Tan <tj8901nm@gmail.com>
2016-11-17 15:02:26 -0500

    Address comments in PR

--- R/test_activation.R
+++ R/test_activation.R
@@ -95,17 +95,15 @@ get_active_signatures <- function(limma_result, method = "pareto",
   test_result <- limma_result$test_result
   phenotypes <- limma_result$phenotypes
 
-  if (pheno_group == "both") {
-    test_result$diff <- abs(test_result$diff)
-  } else if (pheno_group == levels(phenotypes)[1]) {
+  if (pheno_group == levels(phenotypes)[1]) {
     test_result <- test_result[test_result$diff <= 0, ]
-    test_result$diff <- abs(test_result$diff)
   } else if (pheno_group == levels(phenotypes)[2]) {
     test_result <- test_result[test_result$diff >= 0, ]
-  } else{
+  } else if (!pheno_group == "both") {
     stop("pheno_group can only be \"both\" or one of the two phenotypes used
          in the run_limma function!")
   }
+  test_result$diff <- abs(test_result$diff)
 
   if (method == "pvalue") {
     active_signatures <- test_result$signature[
